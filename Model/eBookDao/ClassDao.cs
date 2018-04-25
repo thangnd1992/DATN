8668a50c44469814ed6ba5b5c59262a488d06187
ThangND <thangnd260392@gmail.com>
2018-04-25 18:57:39 +0700

    class, layout

---
+++ Model/eBookDao/ClassDao.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.eBookData;
+using PagedList;
+
+namespace Model.eBookDao
+{
+    public class ClassDao
+    {
+        eBooks eb = null;
+        public ClassDao()
+        {
+            eb = new eBooks();
+        }
+        public Class InsertClass(Class cl)
+        {
+            cl.ClassStatus = "active";
+            eb.Class.Add(cl);
+            eb.SaveChanges();
+            return cl;
+        }
+
+        public Class UpdateClass(Class cl)
+        {
+            var Class = GetClassById(cl.Id);
+            Class.ClassCode = cl.ClassCode;
+            Class.ClassName = cl.ClassName;
+            Class.ClassStatus = "active";
+            eb.SaveChanges();
+
+            return cl;
+        }
+
+        public Class GetClassById(int Id)
+        {
+            var Class = eb.Class.Where(o => o.Id == Id).FirstOrDefault();
+            if (Class != null)
+                return Class;
+            return new Class();
+        }
+        public Class GetClassByUserName(string classCode)
+        {
+            var Class = eb.Class.Where(o => o.ClassCode == classCode).FirstOrDefault();
+            if (Class != null)
+                return Class;
+            return null;
+        }
+        public List<Class> GetClasss()
+        {
+            return eb.Class.ToList();
+        }
+        public IEnumerable<Class> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Class> model = eb.Class;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.ClassCode.Contains(searchString) || x.ClassName.Contains(searchString));
+            }
+
+            return model.OrderByDescending(x => x.Id).ToPagedList(page, pageSize);
+        }
+    }
+    
+}
---
+++ Model/eBookData/Class.cs
@@ -0,0 +1,25 @@
+﻿namespace Model.eBookData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Class")]
+    public partial class Class
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Mã lớp không được để trống!")]
+        public string ClassCode { get; set; }
+
+        [StringLength(250)]
+        [Required(ErrorMessage = "Tên lớp không được để trống!")]
+        public string ClassName { get; set; }
+
+        [StringLength(10)]
+        public string ClassStatus { get; set; }
+    }
+}
